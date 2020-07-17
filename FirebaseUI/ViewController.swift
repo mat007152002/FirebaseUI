a55bb9f89a1034f402bd06e629e0c6f091de63e3
Matthew Ling <mat007152002g@gmail.com>
2020-07-17 10:26:24 +0800

    Initial Commit

---
+++ FirebaseUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseUI
+//
+//  Created by 旌榮 凌 on 2020/7/17.
+//  Copyright © 2020 com.matthewProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
