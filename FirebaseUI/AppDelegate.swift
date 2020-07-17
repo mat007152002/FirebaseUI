9bdafd859831208b7f3e2dbad70e90aaa4b4e510
Matthew Ling <mat007152002g@gmail.com>
2020-07-17 14:33:17 +0800

    Google Login done

--- FirebaseUI/AppDelegate.swift
+++ FirebaseUI/AppDelegate.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 import CoreData
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        return GIDSignIn.sharedInstance().handle(url)
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
--- FirebaseUI/ViewController.swift
+++ FirebaseUI/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInDelegate{
 
+    @IBOutlet weak var loginMessage: UILabel!
+    @IBOutlet weak var GoogleSignInBtn: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance()?.delegate = self
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error == nil{
+//            if let userName = user.profile.name {
+//                loginMessage.text = "Hello \(userName)"
+//            }
+            if let autentication = user.authentication{
+                let credential = GoogleAuthProvider.credential(withIDToken: autentication.idToken, accessToken: autentication.accessToken)
+                
+                Auth.auth().signIn(with: credential, completion: { (firebaseuser, error) in if error == nil {
+                    if let username = firebaseuser?.user.displayName{
+                        self.loginMessage.text = "Hello Firebase User: \(username)"
+                        self.GoogleSignInBtn.isEnabled = false
+                        let alertView = UIAlertController.init(title: "登入成功", message: "歡迎: \(username)", preferredStyle: UIAlertController.Style.alert)
+                        alertView.addAction(UIAlertAction.init(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alertView, animated: true, completion: nil)
+                    }
+                    
+                }else{
+                    print(error?.localizedDescription)
+                    let alertView = UIAlertController.init(title: "登入失敗", message: "原因：\(error!.localizedDescription)", preferredStyle: UIAlertController.Style.alert)
+                    alertView.addAction(UIAlertAction.init(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alertView,animated: true,completion: nil)
+                    }
+                    
+                })
+            }
+            
+        }else{
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+            self.GoogleSignInBtn.isEnabled = true
+            self.loginMessage.text = "請選擇登入方式"
+        }catch{
+            print(error.localizedDescription)
+        }
     }
 
 
